2d5c013c8775a0e6d3652365431d2bbbcd9310f5
AlinKchhetri <kcalan672@gmail.com>
2023-05-24 23:50:43 +0545

    Init

--- src/App.js
+++ src/App.js
@@ -1,32 +1,288 @@
-import "./App.css"
-import About from "./components/About"
-import Blog from "./components/Blog"
-import Brading from "./components/Brading"
-import Footer from "./components/Footer"
-import Header from "./components/Header"
-import Home from "./components/Home"
-import Portfolio from "./components/portfolio/Portfolio"
-import Skill from "./components/Skill"
-import Testimonials from "./components/Testimonials"
-import Work from "./components/Work"
-import Wrapper from "./components/Wrapper"
+// import "antd/dist/antd.min.css";
+// import { TextField, Button } from "@mui/material";
+// import { Button as AntButton } from "antd";
+import "./AppNew.css";
+import React from "react";
 
-function App() {
+const App = () => {
   return (
-    <>
-      <Header />
-      <Home />
-      <About />
-      <Portfolio />
-      <Skill />
-      <Wrapper />
-      <Brading />
-      <Testimonials />
-      <Blog />
-      <Work />
-      <Footer />
-    </>
-  )
-}
+    <div className="wwwmixoio-by-htmltodesign">
+      <div className="div">
+        <div className="sectiono1lexkudat">
+          <div className="div1">
+            <div className="div2">
+              <img
+                className="file-b109e9fe-fb24-48de-bf10-8-icon"
+                alt=""
+                src="https://img.freepik.com/free-vector/city-driver-concept-illustration_114360-1209.jpg?w=1060&t=st=1676823830~exp=1676824430~hmac=7ced7b2c1f99c0f423838aa5575edecbca71d21dc7111ea8ff8aec59d0d0d3f9"
+              />
+              <b className="parkpin">ParkPin</b>
+            </div>
+            <div className="div3">
+              <div className="div4">
+                <div className="find-and-book-container">
+                  <span className="find-and-book-container1">
+                    <span>Find     and book yourparking spaces with ParkPin</span>
+                    <span className="span">.</span>
+                  </span>
+                </div>
+                <div className="book-parking-easily">
+                  Book parking easily and conveniently with ParkPin. Save time
+                  and money with our quick and easy parking booking app.
+                </div>
+              </div>
+              <div className="form">
+                {/* <TextField
+                  className="inputhero-email"
+                  color="primary"
+                  variant="outlined"
+                  type="text"
+                  label="Enter your email..."
+                  placeholder="Placeholder"
+                  size="medium"
+                  margin="none"
+                /> */}
+                {/* <Button variant="contained" color="primary">
+                  Join our waitlist
+                </Button> */}
+              </div>
+              <div className="div5">
+                <div className="div6">
+                  <img className="svg-icon" alt="" src="../svg.svg" />
+                  <img className="svg-icon" alt="" src="../svg1.svg" />
+                  <img className="svg-icon" alt="" src="../svg2.svg" />
+                  <img className="svg-icon" alt="" src="../svg3.svg" />
+                  <img className="svg-icon" alt="" src="../svg4.svg" />
+                </div>
+                <div className="div7">
+                  <div className="rated-5-stars">Rated 5 stars</div>
+                </div>
+                <div className="div8">
+                  <div className="over-1000-happy">Over 1000 happy users</div>
+                </div>
+              </div>
+              <div className="div9">
+                <img
+                  className="female-18png-icon"
+                  alt=""
+                  src="../female18png@2x.png"
+                />
+                <div className="div10">
+                  <div className="parkpin-makes-finding">
+                    “ParkPin makes finding parking easy and convenient!”
+                  </div>
+                  <b className="emma-cary">Emma Cary</b>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="div11">
+            <img className="svg-icon5" alt="" />
+            <div className="div12" />
+            <div className="mobile-login-01svg">
+              <img
+                className="mobile-login-01svg-icon"
+                alt=""
+                src={require('./main.png')}
+              />
+            </div>
+          </div>
+        </div>
+        <div className="section503bx56a6y">
+          <div className="div13">
+            <div className="div14">
+              <div className="div15">
+                <div className="search-for-available-container">
+                  <span className="find-and-book-container1">
+                    <span>Search for available parking</span>
+                    <span className="span">.</span>
+                  </span>
+                </div>
+                <div className="with-parkpin-you">
+                  With ParkPin, you can find and book parking spaces in minutes.
+                  We provide a hassle-free booking experience so you can easily
+                  and conveniently find the perfect spot for you.
+                </div>
+              </div>
+            </div>
+            <div className="software-code-testing-artboard">
+              {/* <img
+                className="software-code-testing-artboard-icon"
+                alt=""
+                src={require('./main2.png')}  
+                            /> */}
+<video className='software-code-testing-artboard-icon' autoPlay loop muted>
+      <source src={require('./Login Previewed.mp4')} type='video/mp4'/>
+</video>
+            </div>
+          </div>
+          <div className="div16">
+            <div className="div17">
+              <div className="div15">
+                <div className="save-time-and-container">
+                  <span className="find-and-book-container1">
+                    <span>Save time and money</span>
+                    <span className="span">.</span>
+                  </span>
+                </div>
+                <div className="our-parking-booking">
+                  Our parking booking app saves you time and money. With
+                  ParkPin, you can quickly find the closest and most affordable
+                  parking spots to save you time and money.
+                </div>
+              </div>
+            </div>
+            <div className="cooking-artboard-1svg">
+              <img
+                className="software-code-testing-artboard-icon"
+                alt=""
+                src={require('./main3.png')}  
+              />
+            </div>
+          </div>
+        </div>
+        <div className="sectionopm85he4tt">
+          <div className="div19">
+            <img className="svg-icon6" alt="" />
+            <div className="blockquote">
+              <b className="i-can-always">
+                "I can always find a spot and book it in no time."
+              </b>
+              <div className="div20">
+                <img
+                  className="female-21png-icon"
+                  alt=""
+                  src="../female21png@2x.png"
+                />
+                <div className="teresa-tinkler">Teresa Tinkler</div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="div21">
+          <div className="questions">Questions?</div>
+          <div className="dl">
+            <div className="div22">
+              <div className="how-much-does">How much does it cost?</div>
+              <div className="provide-your-potential">
+                Provide your potential customers with an overview of how much
+                they would be paying.
+              </div>
+            </div>
+            <div className="div23">
+              <div className="who-is-this">Who is this for?</div>
+              <div className="explain-the-primary">
+                Explain the primary user / customer that is designed for. Also
+                include some benefits that may specifically relate to this
+                customer base.
+              </div>
+            </div>
+            <div className="div24">
+              <div className="what-makes-this">What makes this different?</div>
+              <div className="include-details-that">
+                Include details that give a competitive advantage over what else
+                is out there in the market.
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="section6xemailcil">
+          <div className="div25" />
+          <img className="svg-icon7" alt="" />
+          <div className="div26">
+            <img className="svg-icon8" alt="" src="../svg5.svg" />
+            <div className="div27">
+              <div className="div28">
+                <img
+                  className="ben7656dd36jpeg-icon"
+                  alt=""
+                  src="../ben7656dd36jpeg@2x.png"
+                />
+                <img className="image-icon" alt="" src="../image@2x.png" />
+                <img
+                  className="clairebfdbc161jpeg-icon"
+                  alt=""
+                  src="../clairebfdbc161jpeg@2x.png"
+                />
+                <img
+                  className="iwan70e1c572jpeg-icon"
+                  alt=""
+                  src="../iwan70e1c572jpeg@2x.png"
+                />
+                <img
+                  className="lori07099057jpeg-icon"
+                  alt=""
+                  src="../lori07099057jpeg@2x.png"
+                />
+                <img
+                  className="malia8248c71webp-icon"
+                  alt=""
+                  src="../malia8248c71webp@2x.png"
+                />
+                <img
+                  className="mi68a0f720jpeg-icon"
+                  alt=""
+                  src="../mi68a0f720jpeg@2x.png"
+                />
+                <img
+                  className="nimd6bbfb63jpeg-icon"
+                  alt=""
+                  src="../nimd6bbfb63jpeg@2x.png"
+                />
+                <img
+                  className="san3b86301bjpeg-icon"
+                  alt=""
+                  src="../san3b86301bjpeg@2x.png"
+                />
+                <img
+                  className="sanjid2d7a907fjpeg-icon"
+                  alt=""
+                  src="../sanjid2d7a907fjpeg@2x.png"
+                />
+                <img
+                  className="steph68f47994jpeg-icon"
+                  alt=""
+                  src="../steph68f47994jpeg@2x.png"
+                />
+                <img
+                  className="zakbe2e6aebjpeg-icon"
+                  alt=""
+                  src="../zakbe2e6aebjpeg@2x.png"
+                />
+                <img
+                  className="judith8d0417f0jpeg-icon"
+                  alt=""
+                  src="../judith8d0417f0jpeg@2x.png"
+                />
+              </div>
+              <div className="join-our-community-container">
+                <span className="find-and-book-container1">
+                  <span>Join our community of fans that love ParkPin</span>
+                  <span className="span">.</span>
+                </span>
+              </div>
+              <div className="form1">
+                {/* <TextField
+                  className="inputhero-email"
+                  color="primary"
+                  variant="outlined"
+                  type="text"
+                  label="Enter your email..."
+                  placeholder="Placeholder"
+                  size="medium"
+                  margin="none"
+                /> */}
+                {/* <AntButton type="primary" size="middle" shape="default">
+                  Join the waiting list
+                </AntButton> */}
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default App
+export default App;
+
